e85e1383b25210fe726b92850131ddbb9b3f9b74
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-06 20:05:12 +0800

    feat(w1p1): encrypt password before saving into db

--- students/yin-hsiang/Canchu/src/account/signup.ts
+++ students/yin-hsiang/Canchu/src/account/signup.ts
@@ -1,10 +1,14 @@
+import assert from "assert";
+
 import express from "express";
 import z from "zod";
 import mysql from 'mysql2';
-import assert from "assert";
+import bcrypt from 'bcrypt';
 
 /// <reference path="../util/types/apiResponse.d.ts" />
 import * as jwt from "./jwt.js";
+
+const saltRounds = 10;
 
 export default function (sql: mysql.Connection) {
   return function (req: express.Request, res: express.Response, next: express.NextFunction): void {
@@ -20,7 +24,7 @@ export default function (sql: mysql.Connection) {
 
     const parsedReq = bodySchema.safeParse(req.body);
     if (parsedReq.success) {
-      sql.query("SELECT email FROM user WHERE email = ?", [parsedReq.data.email], function (err, result, fields) {
+      sql.query("SELECT email FROM user WHERE email = ?", [parsedReq.data.email], async function (err, result, fields) {
         if (err) {
           console.warn(`sql query error on 'SELECT email FROM user WHERE email = ${parsedReq.data.email}'`, err);
           res.status(500).send("sql error");
@@ -31,7 +35,7 @@ export default function (sql: mysql.Connection) {
             res.status(403).send({ error: `${parsedReq.data.email} already registered` });
           } else {
             sql.query("INSERT INTO user (provider, name, email, picture, password) VALUES ('', ?, ?, '', ?)",
-              [parsedReq.data.name, parsedReq.data.email, parsedReq.data.password],
+              [parsedReq.data.name, parsedReq.data.email, await bcrypt.hash(parsedReq.data.password, saltRounds)],
               function (err, result: mysql.ProcedureCallPacket<mysql.ResultSetHeader>, fields) {
                 if (err) {
                   res.status(403).send({ error: `${parsedReq.data.email} already registered` });
@@ -40,6 +44,7 @@ export default function (sql: mysql.Connection) {
                     [result.insertId],
                     async function (err, result, fields) {
                       assert(err === null);
+                      // {[key: string]: any}只是為了滿足型態檢查，實際上不會沒事接受任意key
                       const usrObj = result as unknown as ({[key: string]: any} & Canchu.Api.Res.IUserObject)[];
                       res.status(200).send({
                         "access_token": await jwt.encode(usrObj[0]),
