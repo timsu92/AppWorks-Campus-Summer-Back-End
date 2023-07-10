e9fa2f83474fe0eb427930a84d14775e1589c2ff
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-10 10:53:51 +0800

    feat(w1p3,profile): finish user's profile update api

--- students/yin-hsiang/Canchu/src/index.ts
+++ students/yin-hsiang/Canchu/src/index.ts
@@ -5,7 +5,7 @@ import mysql from 'mysql2';
 import env from '../.env.json' assert { type: "json" };
 import signup from './users/signup.js';
 import signin from './users/signin.js';
-import { getUserProfile } from './users/profile.js';
+import { getUserProfile, updateUserProfile } from './users/profile.js';
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,6 +19,7 @@ app.get('/', function (req, res) {
 app.post('/api/1.0/users/signup', signup(sql));
 app.post('/api/1.0/users/signin', signin(sql));
 app.get('/api/1.0/users/:id/profile', getUserProfile(sql));
+app.put('/api/1.0/users/profile', updateUserProfile(sql));
 
 app.listen(port, () => {
   console.log(`Canchu backend listening on port:${port}`);
--- students/yin-hsiang/Canchu/src/users/profile.ts
+++ students/yin-hsiang/Canchu/src/users/profile.ts
@@ -1,22 +1,22 @@
 import express from 'express';
 import mysql from 'mysql2';
-import z from 'zod';
 import jose from 'jose';
+import z from "zod";
 
 import { CanchuZod } from '../util/types/api.js';
 import * as jwt from './jwt.js';
 
-type oSuccess = {
-  "data": {
-    "user": Canchu.IUserDetailObject
+export function getUserProfile(sql: mysql.Connection | mysql.Pool) {
+  type oSuccess = {
+    "data": {
+      "user": Canchu.IUserDetailObject
+    }
   }
-}
 
-type oError = {
-  "error": string
-}
+  type oError = {
+    "error": string
+  }
 
-export function getUserProfile (sql: mysql.Connection | mysql.Pool) {
   return async function (req: express.Request, res: express.Response<oSuccess | oError>, next: express.NextFunction): Promise<void> {
     if (Number.isNaN(+req.params.id)) {
       res.status(400).send({ "error": "invalid id" });
@@ -38,6 +38,67 @@ export function getUserProfile (sql: mysql.Connection | mysql.Pool) {
           }
         });
       }
+    } catch (err) {
+      res.status(403).send({ "error": "Can't parse token" });
+    }
+  }
+}
+
+export function updateUserProfile(sql: mysql.Connection | mysql.Pool) {
+  const iBodyZ = z.object({
+    "name": z.string().nonempty(),
+    "introduction": z.string(),
+    "tags": z.string()
+  })
+
+  type oSuccess = {
+    "data": {
+      "user": {
+        "id": number
+      }
+    }
+  }
+
+  type oError = {
+    "error": string
+  }
+
+  return async function (req: express.Request, res: express.Response<oSuccess | oError>, next: express.NextFunction): Promise<void> {
+    if (req.headers["content-type"] !== "application/json") {
+      res.status(400).send({ error: "invalid content type" });
+      return;
+    }
+    const access_token = (req.headers["authorization"] ?? "").match(/(?<=^Bearer ).+/)?.[0];
+    if (access_token === undefined) {
+      res.status(401).send({ "error": "No token" });
+      return;
+    }
+    try {
+      const usrDetailObj = CanchuZod.UserDetailObject.safeParse(
+        (await jwt.decode(access_token)).payload
+      );
+      if (!usrDetailObj.success) {
+        res.status(403).send({ "error": "invalid token format" });
+        return;
+      }
+      const body = iBodyZ.safeParse(req.body);
+      if (!body.success) {
+        res.status(400).send({ "error": body.error.message });
+        return;
+      }
+      sql.query("UPDATE user SET name=?, introduction=?, tags=? WHERE id=?",
+        [body.data.name, body.data.introduction, body.data.tags, usrDetailObj.data.id],
+        function (err, result: mysql.ProcedureCallPacket<mysql.ResultSetHeader>) {
+          if (err) {
+            res.status(500).send({ "error": "internal database error" });
+            console.error(`error while executing 'UPDATE user SET name=${body.data.name}, introduction=${body.data.introduction}, tags=${body.data.tags} WHERE id=${usrDetailObj.data.id}'\n`, err);
+          } else if (result.affectedRows === 0) {
+            res.status(403).send({ "error": "Wrong token" });
+          } else {
+            res.status(200).send({ "data": { "user": { "id": usrDetailObj.data.id } } });
+            console.log(`user with id ${usrDetailObj.data.id} changed profile to ${body.data}`);
+          }
+        })
     } catch (err) {
       res.status(403).send({ "error": "Can't parse token" });
     }
