d88a5fa9d1918ba28225e681948b84a241af8d7c
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-10 16:47:56 +0800

    refactor(w1p2,signin): rename variable

--- students/yin-hsiang/Canchu/src/users/signin.ts
+++ students/yin-hsiang/Canchu/src/users/signin.ts
@@ -46,16 +46,16 @@ export default function (sql: mysql.Connection | mysql.Pool) {
               res.status(500).send({ "error": "internal database error" });
               console.error(`error while sql executing 'SELECT id,provider,email,name,picture,password FROM user WHERE email=${parsedData.email}'\n`, err);
             } else {
-              let qryResult = result as (Canchu.IUserObject & {"password": string})[];
-              if (qryResult.length === 0) {
+              let usrObj = result as (Canchu.IUserObject & {"password": string})[];
+              if (usrObj.length === 0) {
                 res.status(403).send({ "error": "User Not Found" });
-              } else if (await bcrypt.compare(parsedData.password, qryResult[0].password)) {
+              } else if (await bcrypt.compare(parsedData.password, usrObj[0].password)) {
                 // @ts-expect-error
-                delete qryResult[0].password;
+                delete usrObj[0].password;
                 res.status(200).send({
                   "data": {
-                    "access_token": await jwt.encode({ "id": qryResult[0].id }),
-                    "user": qryResult[0]
+                    "access_token": await jwt.encode({ "id": usrObj[0].id }),
+                    "user": usrObj[0]
                   }
                 });
                 next();
