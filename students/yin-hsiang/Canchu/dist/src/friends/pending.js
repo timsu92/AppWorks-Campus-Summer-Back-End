32966639218749aaca18558c1f62bf5627fd530f
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-12 09:53:25 +0000

    chore: compile ts for chatgpt

---
+++ students/yin-hsiang/Canchu/dist/src/friends/pending.js
@@ -0,0 +1,23 @@
+import { Friendship } from '../db/entity/friendship.js';
+export default async function (req, res, next) {
+    const receiverId = req.body.loginUserId;
+    const friendships = await Friendship.findBy({ receiverId, "status": "pending" });
+    res.status(200).send({
+        "data": {
+            "users": friendships.map(relation => {
+                return {
+                    "id": relation.requester.id,
+                    "name": relation.requester.name,
+                    "picture": relation.requester.picture,
+                    "friendship": {
+                        "id": relation.id,
+                        "status": relation.status
+                    }
+                };
+            })
+        }
+    });
+    console.log(`user ${receiverId} listed pending friends`);
+    next();
+}
+//# sourceMappingURL=pending.js.map
