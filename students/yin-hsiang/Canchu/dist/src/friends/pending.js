59ec0b506c04369dbb1d36def4f77dd7390bc14c
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-12 18:22:38 +0800

    chore: compile ts for chatgpt

--- students/yin-hsiang/Canchu/dist/src/friends/pending.js
+++ students/yin-hsiang/Canchu/dist/src/friends/pending.js
@@ -2,9 +2,6 @@ import { Friendship } from '../db/entity/friendship.js';
 export default async function (req, res, next) {
     const receiverId = req.body.loginUserId;
     const friendships = await Friendship.find({ "where": { receiverId, "status": "requested" }, "relations": ["requester"] });
-    friendships.map(relation => {
-        console.log(relation);
-    });
     res.status(200).send({
         "data": {
             "users": friendships.map(relation => {
@@ -14,7 +11,7 @@ export default async function (req, res, next) {
                     "picture": relation.requester.picture,
                     "friendship": {
                         "id": relation.id,
-                        "status": relation.status
+                        "status": "pending"
                     }
                 };
             })
