4bb35fb315cdaa811632bd5bee1f165e4494aeb0
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-14 14:24:35 +0800

    fix(w2p3): avoid hour overflow

--- students/yin-hsiang/Canchu/src/util/util.ts
+++ students/yin-hsiang/Canchu/src/util/util.ts
@@ -1,5 +1,9 @@
 export function date2CanchuStr (timestamp: Date) {
-  const [date, time] = timestamp.toISOString().split('T');
-  const [hour, minute, second] = time.split('.')[0].split(':');
-  return `${date} ${+hour + 8}:${minute}:${second}`;
+  const year = timestamp.getFullYear();
+  const month = (timestamp.getMonth() + 1).toString().padStart(2, '0');
+  const date = timestamp.getDate().toString().padStart(2, '0');
+  const hour = (timestamp.getHours()).toString().padStart(2, '0');
+  const minute = timestamp.getMinutes().toString().padStart(2, '0');
+  const second = timestamp.getSeconds().toString().padStart(2, '0');
+  return `${year}-${month}-${date} ${hour}:${minute}:${second}`;
 }
