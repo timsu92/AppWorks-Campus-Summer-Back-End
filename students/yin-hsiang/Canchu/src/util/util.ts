ae0d5ca24cd6da235a6e19441be47c9bf010c6cd
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-15 21:04:05 +0800

    fix(w2p3): convert time generate by MySQL to local time

--- students/yin-hsiang/Canchu/src/util/util.ts
+++ students/yin-hsiang/Canchu/src/util/util.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 
+const jetlagMillisecond = 1000 * 60 * (new Date().getTimezoneOffset() * -1);
+
 export function date2CanchuStr(timestamp: Date) {
-  const year = timestamp.getFullYear();
-  const month = (timestamp.getMonth() + 1).toString().padStart(2, '0');
-  const date = timestamp.getDate().toString().padStart(2, '0');
-  const hour = (timestamp.getHours()).toString().padStart(2, '0');
-  const minute = timestamp.getMinutes().toString().padStart(2, '0');
-  const second = timestamp.getSeconds().toString().padStart(2, '0');
+  const localTime = new Date(timestamp.valueOf() + jetlagMillisecond);
+  const year = localTime.getFullYear();
+  const month = (localTime.getMonth() + 1).toString().padStart(2, '0');
+  const date = localTime.getDate().toString().padStart(2, '0');
+  const hour = (localTime.getHours()).toString().padStart(2, '0');
+  const minute = localTime.getMinutes().toString().padStart(2, '0');
+  const second = localTime.getSeconds().toString().padStart(2, '0');
   return `${year}-${month}-${date} ${hour}:${minute}:${second}`;
 }
 
