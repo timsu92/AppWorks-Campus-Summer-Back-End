724aa7afe30ce820d6021590b64c4900abcd799e
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-20 15:11:56 +0800

    fix(w3p4,user_profile): retrieve previously skipped friendship

--- students/yin-hsiang/Canchu/src/users/profile.ts
+++ students/yin-hsiang/Canchu/src/users/profile.ts
@@ -2,7 +2,10 @@ import express from 'express';
 import mysql from 'mysql2';
 import jose from 'jose';
 import z from "zod";
+import { Equal } from 'typeorm';
 
+import { User } from '../db/entity/user.js';
+import { Friendship } from '../db/entity/friendship.js';
 import * as jwt from './jwt.js';
 import { AccessTokenSuccessBody } from './auth.js';
 
@@ -22,34 +25,50 @@ export function getUserProfile(sql: mysql.Connection | mysql.Pool) {
     res: express.Response<oSuccess | oError>,
     next: express.NextFunction
   ) {
-    if (Number.isNaN(+req.params.id)) {
+    const targetUserId = +req.params.id;
+    if (Number.isNaN(targetUserId)) {
       res.status(400).send({ "error": "invalid id" });
       return;
     }
-    sql.query("SELECT id,name,picture,introduction,tags FROM user WHERE id=?",
-      [req.params.id],
-      function (err, result, fields) {
-        if (err) {
-          res.status(500).send({ "error": "internal database error" });
-          console.error(`error while executing "SELECT id,name,picture,introduction,tags FROM user WHERE id=${req.params.id}"`);
-          return;
+    const usr = await User.findOne({
+      "select": {
+        "id": true, "name": true, "picture": true, "introduction": true, "tags": true,
+      },
+      "where": { "id": Equal(targetUserId) },
+    });
+    if (usr === null) {
+      res.status(404).send({ "error": "user not found" });
+      return;
+    }
+    const friendship = (await Friendship.findOne({
+      "select": { "id": true, "requesterId": true, "receiverId": true, "status": true },
+      "where": [
+        { "requesterId": Equal(targetUserId), "receiverId": Equal(req.body.loginUserId) },
+        { "requesterId": Equal(req.body.loginUserId), "receiverId": Equal(targetUserId) },
+      ]
+    }));
+    console.log(`user ${targetUserId}'s profile was got'`);
+    res.status(200).send({
+      "data": {
+        "user": {
+          "id": usr.id,
+          "name": usr.name,
+          "picture": usr.picture,
+          "friend_count": 0, // TODO: get friend count <20-07-23, timsu92> //
+          "introduction": usr.introduction,
+          "tags": usr.tags,
+          "friendship": friendship === null
+            ? null
+            : {
+              "id": friendship.id,
+              "status": friendship.status === "friend"
+                ? "friend"
+                : req.body.loginUserId === friendship.requesterId ? "requested" : "pending"
+            }
         }
-        const usrDetailObjs = (result as Canchu.IUserDetailObject[]).map(fakeObj => {
-          return {
-            ...fakeObj,
-            "friend_count": 0,
-            "friendship": null
-          }
-        });
-        if (usrDetailObjs.length !== 1) {
-          res.status(404).send({ "error": "user not found" });
-          return;
-        }
-        console.log(`user ${usrDetailObjs[0].id}'s profile was got`)
-        res.status(200).send({ "data": { "user": usrDetailObjs[0] } });
-        next();
       }
-    )
+    });
+    next();
   }
 }
 
