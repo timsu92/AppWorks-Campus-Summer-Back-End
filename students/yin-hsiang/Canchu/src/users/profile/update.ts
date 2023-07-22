514150156acf42b5604d9ab507e6ded766b15f57
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-22 13:30:17 +0800

    refactor(w3p4,user_profile): use global content type and auth

--- students/yin-hsiang/Canchu/src/users/profile/update.ts
+++ students/yin-hsiang/Canchu/src/users/profile/update.ts
@@ -1,9 +1,8 @@
 import express from 'express';
 import mysql from 'mysql2';
-import jose from 'jose';
 import z from "zod";
 
-import * as jwt from '../jwt.js';
+import { AccessTokenSuccessBody } from '../auth.js';
 
 export function updateUserProfile(sql: mysql.Connection | mysql.Pool) {
   const iBodyZ = z.object({
@@ -11,6 +10,12 @@ export function updateUserProfile(sql: mysql.Connection | mysql.Pool) {
     "introduction": z.string(),
     "tags": z.string()
   })
+
+  type iBody = {
+    "name": any, // string
+    "introduction": any, // string
+    "tags": any, // string
+  }
 
   type oSuccess = {
     "data": {
@@ -24,43 +29,28 @@ export function updateUserProfile(sql: mysql.Connection | mysql.Pool) {
     "error": string
   }
 
-  return async function (req: express.Request, res: express.Response<oSuccess | oError>, next: express.NextFunction): Promise<void> {
-    if (req.headers["content-type"] !== "application/json") {
-      res.status(400).send({ error: "invalid content type" });
-      return;
-    }
-    const access_token = (req.headers["authorization"] ?? "").match(/(?<=^Bearer ).+/)?.[0];
-    if (access_token === undefined) {
-      res.status(401).send({ "error": "No token" });
-      return;
-    }
-    let payload = { "id": 0 };
-    try {
-      payload = (await jwt.decode(access_token)).payload as { "id": number } & jose.JWTPayload;
-    } catch (err) {
-      res.status(403).send({ "error": "Can't parse token" });
-      return;
-    }
-    if (!z.number().nonnegative().int().safeParse(payload.id).success) {
-      res.status(403).send({ "error": "invalid token format" });
-      return;
-    }
+  return async function (
+    req: express.Request<{}, oSuccess | oError, AccessTokenSuccessBody & iBody>,
+    res: express.Response<oSuccess | oError>,
+    next: express.NextFunction
+  ) {
     const body = iBodyZ.safeParse(req.body);
     if (!body.success) {
       res.status(400).send({ "error": body.error.message });
       return;
     }
+    const userId = req.body.loginUserId;
     sql.query("UPDATE user SET name=?, introduction=?, tags=? WHERE id=?",
-      [body.data.name, body.data.introduction, body.data.tags, payload.id],
+      [body.data.name, body.data.introduction, body.data.tags, userId],
       function (err, result: mysql.ProcedureCallPacket<mysql.ResultSetHeader>) {
         if (err) {
           res.status(500).send({ "error": "internal database error" });
-          console.error(`error while executing 'UPDATE user SET name=${body.data.name}, introduction=${body.data.introduction}, tags=${body.data.tags} WHERE id=${payload.id}'\n`, err);
+          console.error(`error while executing 'UPDATE user SET name=${body.data.name}, introduction=${body.data.introduction}, tags=${body.data.tags} WHERE id=${userId}'\n`, err);
         } else if (result.affectedRows === 0) {
           res.status(403).send({ "error": "Wrong token" });
         } else {
-          res.status(200).send({ "data": { "user": { "id": payload.id } } });
-          console.log(`user with id ${payload.id} changed profile to`, body.data);
+          res.status(200).send({ "data": { "user": { "id": userId } } });
+          console.log(`user with id ${userId} changed profile to`, body.data);
           next();
         }
       })
