3cbd95f203003a8a6d8d15cd8cd0a157b74ae147
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-25 22:44:35 +0800

    feat(w4p2,cache_userProfile): setup cache when getting user profile

--- students/yin-hsiang/Canchu/src/db/cache/common.ts
+++ students/yin-hsiang/Canchu/src/db/cache/common.ts
@@ -1,6 +1,7 @@
 import { init } from "./lib.js";
 import { User } from "./entity/user.js";
+import { Friendship } from "./entity/friendship.js";
 
 export function initDbCache() {
-  init(User);
+  init(User, Friendship);
 }
